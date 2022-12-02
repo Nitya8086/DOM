e672433aaa08c1ffb316ddd8f9f9d3532bbb9850
Nitya8086 <116419245+Nitya8086@users.noreply.github.com>
2022-12-02 23:18:02 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -31,12 +31,6 @@
 
 //GET ITEMS BY CLASS NAME;
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[3]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
 
 items[1].style.backgroundColor = '#f4f4f4';
 
@@ -56,3 +50,9 @@ li[1].style.backgroundColor = '#f4f4f4';
 for(var i=0; i<li.length; i++){
     li[i].style.backgroundColor = '#f4f4f4';
 }
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[3]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'green';
