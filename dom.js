2f2ee290510abe0f31cb064d3ea5247962c158ef
Nitya8086 <116419245+Nitya8086@users.noreply.github.com>
2022-12-03 00:58:28 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -61,6 +61,14 @@ items[2].style.display = 'none';
 var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.color = 'green';
 
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i<odd.length;i++){
+   
+    odd[i].style.backgroundColor = 'green';
+}
+
+
+
 
 
 
