2b44e89c846ff01c2fd5e0f7ccdb009ee6926786
Nitya8086 <116419245+Nitya8086@users.noreply.github.com>
2022-12-02 22:53:13 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -31,18 +31,18 @@
 
 //GET ITEMS BY CLASS NAME;
 
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[3]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[3]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
 
-// items[1].style.backgroundColor = '#f4f4f4';
+items[1].style.backgroundColor = '#f4f4f4';
 
-// for(var i=0; i<items.length; i++){
-//     items[i].style.backgroundColor = '#f4f4f4';
-// }
+for(var i=0; i<items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
 
 var li = document.getElementsByTagName('li');
 console.log(li);
