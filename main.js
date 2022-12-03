38788200bf098ab91f525f90e549fe37fbce247f
Nitya8086 <116419245+Nitya8086@users.noreply.github.com>
2022-12-03 22:44:19 +0530

    Add Dom file

---
+++ main.js
@@ -0,0 +1,70 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+//FORM SUBMIT 
+form.addEventListener('submit',addItem);
+
+//delete item 
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup',filterItems);
+
+// ADD ITEMS
+function addItem(e){
+    e.preventDefault();
+
+    // console.log(1);
+
+    //get input value
+    var newItem = document.getElementById('item').value;
+
+    //create new li element
+    var li = document.createElement('li');
+
+    // add class
+    li.className = "list-group-item";
+    // console.log(li);
+
+    //add text node with input
+    li.appendChild(document.createTextNode(newItem));
+
+    //add delete button
+    var deleteBtn = document.createElement('button');
+
+    //add classes to delete button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+    //creat class
+    deleteBtn.appendChild(document.createTextNode('x'));
+
+    var editBtn = document.createElement('button');
+    editBtn.className = 'edit float-right ';
+
+    editBtn.appendChild(document.createTextNode('edit'));
+   
+    //appent button to li
+    li.appendChild(deleteBtn);
+    li.appendChild(editBtn);
+
+    //appent li to list
+
+
+
+    itemList.appendChild(li);
+}
+
+//remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+    if(confirm('Are u sure ?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+}
+}
+// filter item
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    console.log(text);
+}
+
+
